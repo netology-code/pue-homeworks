94cd1202a2066c66a832883d650867742397ded7
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-07-25 16:18:40 +0300

    Create LMAPlayerController.h

---
+++ 05/LMAPlayerController.h
@@ -0,0 +1,19 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LMAPlayerController.generated.h"
+
+UCLASS()
+class LEAVEMEALONE_API ALMAPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ALMAPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+};
