c50495b759f74f311786f549549cdb6dbe322136
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-07-28 15:33:26 +0300

    Create LMAHealthPickup.cpp

---
+++ 06/LMAHealthPickup.cpp
@@ -0,0 +1,63 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+
+#include "LevelActors/Pickup/LMAHealthPickup.h"
+#include "Components/LMAHealthComponent.h"
+#include "Components/SphereComponent.h"
+#include "Player/LMADefaultCharacter.h"
+
+ALMAHealthPickup::ALMAHealthPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	SetRootComponent(SphereComponent);
+
+}
+
+void ALMAHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALMAHealthPickup::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	const auto Charcter = Cast<ALMADefaultCharacter>(OtherActor);
+	if (GivePickup(Charcter))
+	{
+		PickupWasTaken();
+	}
+}
+
+void ALMAHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+bool ALMAHealthPickup::GivePickup(ALMADefaultCharacter* Character)
+{
+	const auto HealthComponent = Character->GetHealthComponent();
+	if (!HealthComponent) return false;
+
+	return HealthComponent->AddHealth(HealthFromPickup);
+}
+
+void ALMAHealthPickup::PickupWasTaken()
+{
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	GetRootComponent()->SetVisibility(false,true);
+
+	FTimerHandle RespawnTimerHandle;
+	GetWorldTimerManager().SetTimer(RespawnTimerHandle, this, &ALMAHealthPickup::RespawnPickup, RespawnTime);
+}
+
+void ALMAHealthPickup::RespawnPickup()
+{
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	GetRootComponent()->SetVisibility(true,true);
+}
