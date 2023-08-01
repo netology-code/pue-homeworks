4be71691d0cbbf1d3811e7aac0aa37447ac6c30d
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-08-01 17:09:27 +0300

    Create LMADamageActor.h

---
+++ 07/LMADamageActor.h
@@ -0,0 +1,37 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LMADamageActor.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class LEAVEMEALONE_API ALMADamageActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALMADamageActor();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float Damage = 5.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float SphereRadius = 100.0f;
+
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
