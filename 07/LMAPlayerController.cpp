257eb4ea3fa6b673a0814c97db6fbb99654c8137
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-08-01 17:07:56 +0300

    Create LMAPlayerController.cpp

---
+++ 07/LMAPlayerController.cpp
@@ -0,0 +1,24 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+
+#include "Player/LMAPlayerController.h"
+
+
+ALMAPlayerController::ALMAPlayerController()
+{
+}
+
+void ALMAPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetInputMode(FInputModeGameOnly());
+	bShowMouseCursor = false;
+
+}
+
+void ALMAPlayerController::BeginSpectatingState()
+{
+	SetControlRotation(FRotator(-75.0f, 0.0f, 0.0f));
+	Super::BeginSpectatingState();
+}
