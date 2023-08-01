60e10a2a3fd4167c85dd6a8e4940714ec5e29f0b
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-08-01 17:12:28 +0300

    Create LMAWeaponComponent.h

---
+++ 07/LMAWeaponComponent.h
@@ -0,0 +1,47 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LMAWeaponComponent.generated.h"
+
+class ALMABaseWeapon;
+class UAnimMontage;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LEAVEMEALONE_API ULMAWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	ULMAWeaponComponent();
+
+	void Fire();
+	void Reload();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	TSubclassOf<ALMABaseWeapon> WeaponClass;
+
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	UAnimMontage* ReloadMontage;
+
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY()
+	ALMABaseWeapon* Weapon = nullptr;
+
+	bool AnimReloading = false;
+	
+	void SpawnWeapon();
+	void InitAnimNotify();
+
+	void OnNotifyReloadFinished(USkeletalMeshComponent* SkeletalMesh);
+	bool CanReload() const;
+
+};
